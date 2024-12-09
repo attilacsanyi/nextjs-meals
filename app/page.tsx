7b435368de5dcc75ecdc482eca63df2874b49aa7
Attila Csanyi <attila.csnyi@yahoo.co.uk>
2024-12-09 19:05:30 +0100

    feat: enhance homepage and add about page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,22 @@
 import Image from "next/image";
+import Link from "next/link";
 
-export default function Home() {
+const Home = () => {
   return (
     <main>
-      <Image src="/logo.png" width={200} height={200} alt="A server surrounded by magic sparkles." />
+      <Image
+        src="/logo.png"
+        width={200}
+        height={200}
+        alt="A server surrounded by magic sparkles."
+      />
       <h1>Welcome to this NextJS Course!</h1>
+      <p>
+        <Link href="/about">About</Link>
+      </p>
       <p>🔥 Let&apos;s get started! 🔥</p>
     </main>
   );
-}
+};
+
+export default Home;
