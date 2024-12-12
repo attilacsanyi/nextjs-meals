1129bdf03162ef8c53cadf1ef6e488228372208c
Attila Csanyi <attila.csnyi@yahoo.co.uk>
2024-12-12 16:39:19 +0100

    feat: enhance ImagePicker component with image preview functionality and file selection handling

--- components/meals/image-picker.tsx
+++ components/meals/image-picker.tsx
@@ -1,23 +1,44 @@
 "use client";
-import { useRef } from "react";
+import Image from "next/image";
+import { useRef, useState } from "react";
 import classes from "./image-picker.module.css";
 
 const ImagePicker = ({ label, name }: { label: string; name: string }) => {
+  const [pickedImage, setPickedImage] = useState<string | null>(null);
   const imageInputRef = useRef<HTMLInputElement>(null);
 
   const handlePickClickImage = () => {
     imageInputRef.current?.click();
   };
 
+  const handleImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    // We allow only one file to be picked
+    const file = event.target.files?.[0];
+    if (file) {
+      setPickedImage(URL.createObjectURL(file));
+    }
+  };
+
   return (
     <div className={classes.picker}>
       <label htmlFor={name}>{label}</label>
       <div className={classes.controls}>
+        <div className={classes.preview}>
+          {!pickedImage && <p>No image picked yet</p>}
+          {pickedImage && (
+            <Image
+              src={pickedImage}
+              alt="The image selected by the user"
+              fill
+            />
+          )}
+        </div>
         <input
           ref={imageInputRef}
           type="file"
           id={name}
           name={name}
+          onChange={handleImageChange}
           accept="image/png, image/jpeg"
           className={classes.input}
         />
