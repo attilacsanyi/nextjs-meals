2d56dd6d7e36d0a2ad29970d9725bcd8bd769a03
Attila Csanyi <attila.csnyi@yahoo.co.uk>
2024-12-12 15:36:54 +0100

    feat: implement MealPage with styled header and dynamic meal content

--- app/meals/[mealSlug]/page.tsx
+++ app/meals/[mealSlug]/page.tsx
@@ -1,14 +1,42 @@
+import { Meal } from "@/components/meals/types";
+import { getMeal } from "@/lib/meals.mjs";
+import Image from "next/image";
+import classes from "./page.module.css";
+
 const MealPage = async ({
   params,
 }: {
   params: { mealSlug: Promise<string> };
 }) => {
   const mealSlug = (await params).mealSlug;
+  const meal = (await getMeal(mealSlug)) as Meal;
+
+  // Replace newlines with <br /> tags for HTML
+  meal.instructions = meal.instructions.replace(/\n/g, "<br />");
+
   return (
-    <main>
-      <h1>MealPage</h1>
-      <p>{mealSlug}</p>
-    </main>
+    <>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image src={meal.image} fill alt={meal.title} />
+        </div>
+
+        <div className={classes.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={classes.creator}>
+            by <a href={`mailto:${meal.creator_email}`}>{meal.title}</a>
+          </p>
+          <p className={classes.summary}>{meal.summary}</p>
+        </div>
+      </header>
+
+      <main className={classes.main}>
+        <p
+          className={classes.instructions}
+          dangerouslySetInnerHTML={{ __html: meal.instructions }}
+        ></p>
+      </main>
+    </>
   );
 };
 
