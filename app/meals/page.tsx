5c6110aea2be6007ccd781a82824cc570a2ec097
Attila Csanyi <attila.csnyi@yahoo.co.uk>
2024-12-12 11:24:22 +0100

    feat: create MealsPage with styled header and meals grid; add CSS for layout and animations

--- app/meals/page.tsx
+++ app/meals/page.tsx
@@ -1,5 +1,21 @@
+import Link from "next/link";
+import classes from "./page.module.css";
+
 const MealsPage = () => {
-  return <h1>MealsPage</h1>;
+  return (
+    <header className={classes.header}>
+      <h1>
+        Delicious meals, created{" "}
+        <span className={classes.highlight}>by you</span>
+      </h1>
+      <p>
+        Chose your favourite recipe and cook it yourself. It is easy and fun!
+      </p>
+      <p className={classes.cta}>
+        <Link href="/meals/share">Share Your Favourite Recipe</Link>
+      </p>
+    </header>
+  );
 };
 
 export default MealsPage;
